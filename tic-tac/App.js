9d9eeb8f91618606845fda3560b15bba97a851ad
Jeremy Robinson <jezza_0196@hotmail.com>
2026-03-09 19:39:01 +1000

    Tic-tac-toe start

--- tic-tac/App.js
+++ tic-tac/App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
+
+// create a 3x3 grid for tic-tac-toe
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.grid}>
+      <View style={styles.row}>
+        <Pressable style={styles.cell}>
+          <Text style={styles.cellText}>X</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.row}>
+        <Pressable style={styles.cell}>
+          <Text style={styles.cellText}>O</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 10,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  grid: {
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: "#000",
+    padding: 5,
+  }
 });
